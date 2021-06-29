b49784d8bd066f1b813e5b98fab3bf40a857ec80
akolol40 <akolol50@gmail.com>
2021-06-29 22:06:02 +0300

    Update [name].js

--- pages/card/[name].js
+++ pages/card/[name].js
@@ -1,1 +1,14 @@
-dsadsadsadas
+export async function getEdgeProps({ params }) {
+  const { name } = params;
+  const post = await getSomeRemotePost({ slug });
+
+  return {
+    props: {
+      post
+    }
+  }
+}
+
+export default function Post({ post }) {
+   return post
+}
